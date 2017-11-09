db491a675b3a74c1398b25abf21e413e2a7a2f36
Guillermo\Memo <guillermo.narvaez.21.95@gmail.com>
2017-11-09 15:07:57 -0500

    He insertardo Completamente un registro completo de un usuario

--- vistas/Usuario/insertar.php
+++ vistas/Usuario/insertar.php
@@ -2,13 +2,24 @@
 
 include($_SERVER['DOCUMENT_ROOT']."/ProyectoWebJmAutomotriz/controlador/UsuariosControlador.php");
 
-$usuariosCon = new UsuariosControlador();
 
-if(isset($_GET["usuario"]) || isset($_GET["contrasena"])){
 
-    if($usuariosCon->insertarUsuarios(
+if(isset($_GET["usuario"]) || isset($_GET["contrasena"]) 
+|| isset($_GET["nombre"]) || isset($_GET["apellidoPaterno"]) 
+|| isset($_GET["apellidoMaterno"]) || isset($_GET["dni"])){
+    $usuariosCon = new UsuariosControlador();
+    if($usuariosCon->insertarUsuario(
+        $_GET["rol"],
         $_GET["usuario"],
-        $_GET["password"]))
+        $_GET["contrasena"],
+        $_GET["nombre"],
+        $_GET["apellidoPaterno"],
+        $_GET["apellidoMaterno"],
+        $_GET["dni"],
+        $_GET["sexo"],
+        $_GET["telefono"],
+        $_GET["domicilio"]
+        ))
         {
         echo "true<br>";
         echo $_GET["usuario"]."<br>";
--- vistas/Usuario/js/code.js
+++ vistas/Usuario/js/code.js
@@ -8,20 +8,36 @@ function ini(){
 function enviarDatos(){
     var usuario=$("#usuario").val();
     var contrasena=$("#contrasena").val();
-    $('select#rol').on('change',function(){
+    var rol=$("#rol").val();
+    /*
+    $('#rol').on('change',function(){
         var rol = $(this).val();
         alert(rol);
     });
+    */
     var nombre=$("#nombre").val();
     var apellidoPaterno=$("#apellidoPaterno").val();
     var apellidoMaterno=$("#apellidoMaterno").val();
-    var dni=$("#dni");
+    var dni=$("#dni").val();
+    var sexo = $("#sexo").val();
+    /*
     $('select#sexo').on('change',function(){
         var sexo = $(this).val();
         alert(sexo);
     })
+    */
     var telefono=$("#telefono").val();
     var domicilio=$("#domicilio").val();
+    console.log(usuario);
+    console.log(contrasena);
+    console.log(rol);
+    console.log(nombre);
+    console.log(apellidoPaterno);
+    console.log(apellidoMaterno);
+    console.log(dni);
+    console.log(sexo);
+    console.log(telefono);
+    console.log(domicilio);
     $.ajax({
         url:"insertar.php",
         success:function(result){
@@ -31,8 +47,14 @@ function enviarDatos(){
         data:{
             usuario:usuario,
             contrasena:contrasena,
+            rol:rol,
             nombre:nombre,
             apellidoPaterno:apellidoPaterno,
+            apellidoMaterno:apellidoMaterno,
+            dni:dni,
+            sexo:sexo,
+            telefono:telefono,
+            domicilio:domicilio
         },
         type:"GET"
     });
